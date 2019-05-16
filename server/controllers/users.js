4062a57fa9e229599c9a98d221b8b3cbe8636a92
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-05-16 11:55:14 +0300

    feature- add space at end of files

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,10 +1,15 @@
-import jwt from "jsonwebtoken";
-import users from "../db/users";
+import jwt from 'jsonwebtoken';
+import users from '../db/users';
+
 class UsersController {
-  getAllUsers(req, res) {
+  constructor() {
+    this.users = [];
+  }
+
+  getAllUsers(res) {
     res.status(200).json({
       status: 200,
-      data: users
+      data: users,
     });
   }
 
@@ -15,8 +20,6 @@ class UsersController {
       lastname,
       password,
       address,
-      status,
-      isAdmin
     } = req.body;
 
     const userExists = users.find(user => user.email === email);
@@ -24,7 +27,7 @@ class UsersController {
     if (userExists) {
       res.status(400).json({
         status: 400,
-        error: "User already exists"
+        error: 'User already exists',
       });
     }
 
@@ -35,21 +38,21 @@ class UsersController {
     user.lastname = lastname;
     user.password = password;
     user.address = address;
-    user.status = "pending" || "approved";
+    user.status = 'pending';
     user.isAdmin = true || false;
     users.push(user);
     jwt.sign(
       {
         userId: users.id,
         email: users.email,
-        password: users.password
+        password: users.password,
       },
-      "secretkey",
+      'secretkey',
       (error, token) => {
         if (error) {
           return res.status(401).json({
             status: 401,
-            data: "user already exits"
+            data: 'user already exits',
           });
         }
         user.token = token;
@@ -63,16 +66,18 @@ class UsersController {
             lastname: user.lastname,
             password: user.password,
             address: user.address,
-            status: user.status === "rejected" || "approved",
-            isAdmin: user.isAdmin === true || false
-          }
+            status: user.status === 'rejected' || 'approved',
+            isAdmin: user.isAdmin === true || false,
+          },
         };
 
         res.status(200).json(resp);
-      }
+      },
     );
   }
 }
 
+
 const usersController = new UsersController();
 export default usersController;
+
